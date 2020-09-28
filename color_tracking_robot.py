0fecb510d12d9f070bafed9cdf864f06b72c45b0
usui9617 <70953829+usui9617@users.noreply.github.com>
2020-09-28 21:40:34 +0900

    Update color_tracking_robot.py

--- color_tracking_robot.py
+++ color_tracking_robot.py
@@ -1,49 +1,7 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy as np
-import RPi.GPIO as GPIO
-from time import sleep
-GPIO.setmode(GPIO.BCM)
-
-    #----------------moter_assign---------------#
-    #
-    #    FL_moter  / / ｜-----｜ / /  FR_moter
-    #                  ｜     ｜
-    #                  ｜     ｜
-    #    BL_moter  / / ｜-----｜ / /  BR_moter
-    #
-    #-------------------------------------------#
-
-FL_pwm_pin = 26
-FL_dir1 = 19
-
-FR_pwm_pin = 13
-FR_dir1 = 6
-
-BL_pwm_pin = 21
-BL_dir1 = 20
-
-BR_pwm_pin = 16
-BR_dir1 = 12
-
-GPIO.setup(FL_pwm_pin,GPIO.OUT)
-GPIO.setup(FL_dir1,GPIO.OUT)
-
-GPIO.setup(FR_pwm_pin,GPIO.OUT)
-GPIO.setup(FR_dir1,GPIO.OUT)
-
-GPIO.setup(BL_pwm_pin,GPIO.OUT)
-GPIO.setup(BL_dir1,GPIO.OUT)
-
-GPIO.setup(BR_pwm_pin,GPIO.OUT)
-GPIO.setup(BR_dir1,GPIO.OUT)
-
-pwmFL = GPIO.PWM(FL_pwm_pin,200)
-pwmFR = GPIO.PWM(FR_pwm_pin,200)
-pwmBL = GPIO.PWM(BL_pwm_pin,200)
-pwmBR = GPIO.PWM(BR_pwm_pin,200)
-
-#moter drive dir (forward)
+import moter
 
 
 def red_detect(img):
@@ -107,51 +65,30 @@ def main():
         
         # 面積最大ブロブの中心座標を取得
         center_x = int(target["center"][0])
-        center_y = int(target["center"][1])
-        
+        center_y = int(target["center"][1])       
         area = int(target["area"])
-        print(area)
+
         # フレームに面積最大ブロブの中心周囲を円で描く
         cv2.circle(frame, (center_x, center_y), 30, (0, 200, 0),
                    thickness=3, lineType=cv2.LINE_AA)
-        x = center_x - 320
-
+        w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        x = center_x - w/2
         if x > 100:
-                pwmFL.start(15)  
-                pwmBL.start(15)
-                pwmFR.start(15)  
-                pwmBR.start(15)
-                GPIO.output(FL_dir1,True)
-                GPIO.output(FR_dir1,False)  
-                GPIO.output(BL_dir1,True)
-                GPIO.output(BR_dir1,False)
+            #moter_turn_left
+                moter.moter(15,-15,15,-15)
         elif x < -100:
-                pwmFR.start(15)  
-                pwmBR.start(15)
-                pwmFL.start(15)  
-                pwmBL.start(15)
-                GPIO.output(FL_dir1,False)
-                GPIO.output(FR_dir1,True)  
-                GPIO.output(BL_dir1,False)
-                GPIO.output(BR_dir1,True)
-        elif area < 50000:              
-                pwmFR.start(10)  
-                pwmBR.start(10)
-                pwmFL.start(10)  
-                pwmBL.start(10)
-                GPIO.output(FL_dir1,True)
-                GPIO.output(FR_dir1,True)  
-                GPIO.output(BL_dir1,True)
-                GPIO.output(BR_dir1,True)
+            #moter_turn_Right
+                moter.moter(-15,15,-15,15)
+        elif area < 50000: 
+            #moter_straight
+                moter.moter(15,15,15,15)
         else:
-                pwmFR.start(0)  
-                pwmBR.start(0)
-                pwmFL.start(0)  
-                pwmBL.start(0)            
+            #moter_stop
+                moter.moter(0,0,0,0)         
 
         # 結果表示
-        #cv2.imshow("Frame", frame)
-        #print(x)
+        cv2.imshow("Frame", frame)
+        print(x)
         #cv2.imshow("Mask", mask)
 
         # qキーが押されたら途中終了
