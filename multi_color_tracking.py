78ffac8b2cdfad45583da87f0dba53efb529ba63
usui9617 <usui.tomoyasu@astina.co>
2020-10-08 02:03:02 +0900

    add_color_class

--- multi_color_tracking.py
+++ multi_color_tracking.py
@@ -1,87 +1,49 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy as np
-
-def red_detect(img):
-    # HSV色空間に変換
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # 赤色のHSVの値域1
-
-    hsv_min = np.array([0,230,80])
-    hsv_max = np.array([30,255,255])
-
-    mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
-
-    # 赤色のHSVの値域2
-    hsv_min = np.array([150,230,80])
-    hsv_max = np.array([179,255,255])
-    mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
-    
-    return mask1 + mask2
-
-# ブロブ解析
-def analysis_blob(binary_img):
-    # 2値画像のラベリング処理
-    label = cv2.connectedComponentsWithStats(binary_img)
-
-    # ブロブ情報を項目別に抽出
-    n = label[0] - 1
-    data = np.delete(label[2], 0, 0)
-    center = np.delete(label[3], 0, 0)
-
-    # ブロブ面積最大のインデックス
-    max_index = np.argmax(data[:, 4])
-
-    # 面積最大ブロブの情報格納用
-    maxblob = {}
-
-    # 面積最大ブロブの各種情報を取得
-    maxblob["upper_left"] = (data[:, 0][max_index], data[:, 1][max_index]) # 左上座標
-    maxblob["width"] = data[:, 2][max_index]  # 幅
-    maxblob["height"] = data[:, 3][max_index]  # 高さ
-    maxblob["area"] = data[:, 4][max_index]   # 面積
-    maxblob["center"] = center[max_index]  # 中心座標
-    
-    return maxblob
-
-def blue_detect(img):
+class color:
+    def red_detect(img):
         # HSV色空間に変換
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # 赤色のHSVの値域1
+        # 赤色のHSVの値域1
 
-    hsv_min = np.array([90,127,80])
-    hsv_max = np.array([150,255,255])
+        hsv_min = np.array([0,230,80])
+        hsv_max = np.array([30,255,255])
 
-    mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
-    
-    return mask1 
+        mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
 
-def analysis_blob_blue(binary_img):
-    # 2値画像のラベリング処理
-    label = cv2.connectedComponentsWithStats(binary_img)
+        # 赤色のHSVの値域2
+        hsv_min = np.array([150,230,80])
+        hsv_max = np.array([179,255,255])
+        mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
+        
+        return mask1 + mask2
 
-    # ブロブ情報を項目別に抽出
-    #n = label[0] - 1
-    data = np.delete(label[2], 0, 0)
-    center = np.delete(label[3], 0, 0)
+    # ブロブ解析
+    def analysis_blob(binary_img):
+        # 2値画像のラベリング処理
+        label = cv2.connectedComponentsWithStats(binary_img)
 
-    # ブロブ面積最大のインデックス
-    max_index = np.argmax(data[:, 4])
+        # ブロブ情報を項目別に抽出
+        n = label[0] - 1
+        data = np.delete(label[2], 0, 0)
+        center = np.delete(label[3], 0, 0)
 
-    # 面積最大ブロブの情報格納用
-    maxblob = {}
+        # ブロブ面積最大のインデックス
+        max_index = np.argmax(data[:, 4])
 
-    # 面積最大ブロブの各種情報を取得
-    maxblob["upper_left"] = (data[:, 0][max_index], data[:, 1][max_index]) # 左上座標
-    maxblob["width"] = data[:, 2][max_index]  # 幅
-    maxblob["height"] = data[:, 3][max_index]  # 高さ
-    maxblob["area"] = data[:, 4][max_index]   # 面積
-    maxblob["center"] = center[max_index]  # 中心座標
-    
-    return maxblob
+        # 面積最大ブロブの情報格納用
+        maxblob = {}
 
+        # 面積最大ブロブの各種情報を取得
+        maxblob["upper_left"] = (data[:, 0][max_index], data[:, 1][max_index]) # 左上座標
+        maxblob["width"] = data[:, 2][max_index]  # 幅
+        maxblob["height"] = data[:, 3][max_index]  # 高さ
+        maxblob["area"] = data[:, 4][max_index]   # 面積
+        maxblob["center"] = center[max_index]  # 中心座標
+        
+        return maxblob
 
 def main():
     #videofile_path = "C:/github/sample/python/opencv/video/color_tracking/red_pendulum.mp4"
@@ -106,6 +68,7 @@ def main():
         centerb_y = int(targetb["center"][1])       
         area_red = int(target["area"])
         area_blue = int(targetb["area"])
+
 
         # フレームに面積最大ブロブの中心周囲を円で描く
         if area_red > 1000 :
